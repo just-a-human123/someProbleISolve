8f9d87d578a12344c71068aa4a53bb3d655e38c4
alfazari <faa04807@gmail.com>
2022-05-17 09:30:41 +0700

    add: solution to leetcode 13 easy

---
+++ leetcode/13RomanToInteger/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const symbol = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    
+    let number = 0;
+    for (let i = 0; i < s.length; i++) {
+        let current = symbol[s[i]];
+        let next = symbol[s[i+1]];
+        
+        if (current < next) {
+            number += next - current
+            i++
+        } else {
+            number += current
+        }
+    }
+    return number;
+};
